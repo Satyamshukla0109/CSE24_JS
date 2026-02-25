f52740d12ed1162c91d2f305c6612d3bc6bad594
Satyam Shukla <satyamshuklaik@gmail.com>
2026-02-25 15:08:20 +0530

    assignment1

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=20;
+let linebreak="<br>";
+document.write("a+b= ");
+result=a+b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a-b= ");
+result=a-b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a/b= ");
+result=a/b;
+document.write(result);
+document.write(linebreak);
