c69649a35d777d3095a368fbedcd64e49cb919b4
Satyam Shukla <satyamshuklaik@gmail.com>
2026-04-05 17:29:28 +0530

    ASSIGNMENT4 HTML + JS

---
+++ assignment4.js
@@ -0,0 +1,15 @@
+
+var sayHello=function(){
+alert('helloworld!');
+}
+
+var numberArray=[1,2,3];
+console.log(numberArray);
+var animals = ['dog','cat','monkey'];
+console.log(animals);
+var person={
+name:'BarackHusseinObamaII',
+age:'51',
+title:'44thPresidentoftheUnitedStates'
+}
+
