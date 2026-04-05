3f764d7980b2d7a137894f6b6fe5133932f5e078
Satyam Shukla <satyamshuklaik@gmail.com>
2026-04-05 17:58:22 +0530

    operators html + js

---
+++ operators.js
@@ -0,0 +1,59 @@
+
+var a=10;
+var b=20;
+varlinebreak="<br/>";
+document.write("(a==b)=>");
+result=(a==b);
+console.log(result);
+console.log(linebreak);
+console.log("(a<b)=>");
+result=(a<b);
+console.log(result);
+console.log(linebreak);
+console.log("(a>b)=>");
+result=(a>b);
+console.log(result);
+console.log(linebreak);
+console.log("(a!=b)=>");
+result=(a!=b);
+console.log(result);
+console.log(linebreak);
+console.log("(a>=b)=>");
+result=(a>=b);
+console.log(result);
+console.log(linebreak);
+console.log("(a<=b)=>");
+result=(a<=b);
+console.log(result);
+console.log(linebreak);
+
+
+
+
+var a=40;
+var b=20;
+var linebreak="<br/>";
+console.log("Valueofa=>(a=b)=>");
+result=(a=b);
+console.log(result);
+console.log(linebreak);
+console.log("Valueofa=>(a+=b)=>");
+result=(a+=b);
+console.log(result);
+console.log(linebreak);
+console.log("Valueofa=>(a-=b)=>");
+result=(a-=b);
+console.log(result);
+console.log(linebreak);
+console.log("Valueofa=>(a*=b)=>");
+result=(a*=b);
+console.log(result);
+console.log(linebreak);
+console.log("Valueofa=>(a/=b)=>");
+result=(a/=b);
+console.log(result);
+console.log(linebreak);
+console.log("Valueofa=>(a%=b)=>");
+result=(a%=b);
+console.log(result);
+console.log(linebreak);
