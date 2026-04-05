5613d63da7da4f18e6d2850dcb23ac33b7b4c114
Satyam Shukla <satyamshuklaik@gmail.com>
2026-04-05 18:41:25 +0530

    condition js file

---
+++ conditionalstatement.js
@@ -0,0 +1,10 @@
+var age = 16;
+if( age >= 18 ) {
+console.log("<b>You are eligible to cast the vote.</b>");
+}
+else if(age === 17){
+console.log("<b>You are eligible to cast the vote but you have to wait for 1 year.</b>");
+}
+else{
+console.log("<b>You are not eligible to cast the vote.</b>");
+}
