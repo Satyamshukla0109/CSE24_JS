3bac23b6c442988900d4f01f2592453e3f0cab5c
Satyamshukla0109 <satyamshuklaik@gmail.com>
2026-06-19 23:53:59 +0530

    External practical

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-console.log("Hello World!");
-/*var num=null;
-console.log(num);
-console.log(typeof(num));
-num=false;*/
+function calculate(){
+
+    let n = Number(document.getElementById("subjects").value);
+
+    let total = 0;
+
+    for(let i=1;i<=n;i++){
+        let marks = Number(prompt("Enter marks of Subject "+i));
+        total += marks;
+    }
+
+    let avg = total/n;
+
+    let grade;
+
+    if(avg>=90)
+        grade="A";
+    else if(avg>=75)
+        grade="B";
+    else if(avg>=60)
+        grade="C";
+    else
+        grade="D";
+
+    document.getElementById("result").innerHTML=
+    `Total = ${total}<br>
+     Average = ${avg}<br>
+     Grade = ${grade}`;
+}
